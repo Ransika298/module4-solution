51b5a6f9dbc0768440fdf6bb86b852d3b4af444d
Ransika298 <ransikaranawaka73@gmail.com>
2024-08-10 14:20:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Kasun", "John", "Jenny", "Jason", "Piyal", "Dasun", "Lahiru", "Prasad", "Lakindu", "Jimmy"];
     
     for (var i = 0; i < names.length; i++) {
     
