7d56e67450921e9f2aa6de057287913e6f183edd
Ransika298 <ransikaranawaka73@gmail.com>
2024-08-10 13:45:33 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,10 @@
-(function(window) {
-    var byeSpeaker = new Object();
+(function (window){
+    var byeSpeaker = {};
     var speakWord = "Good Bye";
-    byeSpeaker.speak = function speak(name) {
-        console.log(speakWord + " " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
+    
+    byeSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
+    }
+     
+     window.byeSpeaker = byeSpeaker;
+    })(window);
